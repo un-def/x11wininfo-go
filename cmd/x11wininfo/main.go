b8105378b970b2a2fd07eb155fb38c5a6fe2dfa8
un.def <un.def@ya.ru>
2020-03-17 12:18:14 +0300

    Add different output modes

--- cmd/x11wininfo/main.go
+++ cmd/x11wininfo/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 	"x11wininfo/pkg/x11"
 )
+
+var modes = [...]string{
+	"text",
+	"mintext",
+	"json",
+}
+
+type jsonResponse struct {
+	ID       x11.WindowID `json:"id,string"`
+	Name     string       `json:"name"`
+	Instance string       `json:"instance"`
+	Class    string       `json:"class"`
+}
 
 func die(err error, conn *x11.Connection) {
 	fmt.Fprintln(os.Stderr, err)
@@ -14,7 +29,22 @@ func die(err error, conn *x11.Connection) {
 	os.Exit(1)
 }
 
+func isModeSupported(mode string) bool {
+	for _, supportedMode := range modes {
+		if mode == supportedMode {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
+	modePtr := flag.String("m", modes[0], fmt.Sprintf("output `mode`: %v", modes))
+	flag.Parse()
+	mode := *modePtr
+	if !isModeSupported(mode) {
+		die(fmt.Errorf("unsupported mode: %s", mode), nil)
+	}
 	conn, err := x11.Connect()
 	if err != nil {
 		die(err, conn)
@@ -23,6 +53,7 @@ func main() {
 	if err != nil {
 		die(err, conn)
 	}
+	id := window.ID()
 	name, err := window.Name()
 	if err != nil {
 		die(err, conn)
@@ -31,8 +62,28 @@ func main() {
 	if err != nil {
 		die(err, conn)
 	}
-	fmt.Printf("id: %d\n", window.ID())
-	fmt.Printf("name: %s\n", name)
-	fmt.Printf("instance: %s\n", instance)
-	fmt.Printf("class: %s\n", class)
+	switch mode {
+	case "text":
+		fmt.Printf(
+			"id: %d\nname: %s\ninstance: %s\nclass: %s\n",
+			id, name, instance, class,
+		)
+	case "mintext":
+		fmt.Printf(
+			"%d\n%s\n%s\n%s\n",
+			id, name, instance, class,
+		)
+	case "json":
+		response, err := json.Marshal(jsonResponse{
+			ID:       id,
+			Name:     name,
+			Instance: instance,
+			Class:    class,
+		})
+		if err != nil {
+			die(err, conn)
+		}
+		fmt.Println(string(response))
+	}
+
 }
